6f737f8ce4fef4cc4f996d00573125de123196cb
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-11-25 16:45:06 +0200

    Add AOS library for animations, enhance footer layout, and update index page with animation attributes

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,12 @@
 import "../styles/main.scss";
 import ScrollHeader from "./scrollHeader";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 document.addEventListener("DOMContentLoaded", (): void => {
   new ScrollHeader();
+  AOS.init({
+    once: true,
+    duration: 600,
+  });
 });
